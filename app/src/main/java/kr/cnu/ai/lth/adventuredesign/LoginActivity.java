1aa14ccdc42a7759e49e0c1a0621de421a37d199
Talusor <lth030326@naver.com>
2022-12-03 16:08:45 +0900

    로그인 레이아웃 디자인.

--- app/src/main/java/kr/cnu/ai/lth/adventuredesign/LoginActivity.java
+++ app/src/main/java/kr/cnu/ai/lth/adventuredesign/LoginActivity.java
@@ -2,7 +2,9 @@ package kr.cnu.ai.lth.adventuredesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextView forgetPass = findViewById(R.id.forgetPassword);
+        forgetPass.setPaintFlags(forgetPass.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
     }
 }
